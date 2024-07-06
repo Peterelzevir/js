fd8e192b527e3cae52c2dcc5936c634ce798e32d
Peterelzevir <114345532+Peterelzevir@users.noreply.github.com>
2024-07-06 11:18:28 +0700

    Update vcf.js

--- vcf.js
+++ vcf.js
@@ -15,7 +15,7 @@ import stringSimilarity from 'string-similarity';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const bot = new Telegraf('7037157690:AAHIbuJux1MFtdXOwZhvqk-wtnHcSSqoWJM');
+const bot = new Telegraf('7037157690:AAEBfUdtXBgDi3VYRCvb52MIQg8U8W9K5Gg');
 const adminId = '5988451717';  // Ganti dengan ID admin bot
 
 // Fungsi untuk memuat sesi pengguna dari file JSON
