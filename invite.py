a7723eeda07f7e6ee36a6a46676857187c41c1d0
Peterelzevir <114345532+Peterelzevir@users.noreply.github.com>
2024-12-14 20:28:21 +0700

    Update invite.py

--- invite.py
+++ invite.py
@@ -1,6 +1,7 @@
 from telethon import TelegramClient, events
 from telethon.tl.functions.channels import InviteToChannelRequest
-from telethon.tl.functions.users import GetFullUserRequest, GetParticipantsRequest
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.functions.users import GetParticipantsRequest
 from telethon.errors import (
     UserPrivacyRestrictedError,
     UserNotMutualContactError,
